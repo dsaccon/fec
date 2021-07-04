04e9299aa831f6e510d399996a2bc81b5d0fbf5b
dsaccon <>
2021-07-04 01:04:45 +0000

    updated models

--- app/db.py
+++ app/db.py
@@ -35,12 +35,12 @@ class CompanyDB(ormar.Model):
     name: str = ormar.String(max_length=128, unique=True, nullable=False)
     industry: str = ormar.String(max_length=128, unique=False, nullable=False)
     starting_date: str = ormar.String(max_length=128, unique=False, nullable=False)
-    metadata: str = ormar.String(max_length=128, unique=False, nullable=False)
-    statement: bool = ormar.Boolean(nullable=False)
-    broke_promise: bool = ormar.Boolean(nullable=False)
-    created: dt.datetime = ormar.DateTime(nullable=False)
-    last_updated: dt.datetime = ormar.DateTime(nullable=False)
-    active: bool = ormar.Boolean(nullable=False, default=False)
+    metadata: str = ormar.Text(unique=False, nullable=False)
+    statement: bool = ormar.Boolean(unique=False, nullable=False)
+    broke_promise: bool = ormar.Boolean(unique=False, nullable=False)
+    created: dt.datetime = ormar.DateTime(unique=False, nullable=False)
+    last_updated: dt.datetime = ormar.DateTime(unique=False, nullable=False)
+    active: bool = ormar.Boolean(unique=False, nullable=False, default=False)
 
 class CompanyNameAPI(BaseModel):
     name: str
@@ -50,6 +50,9 @@ class CompanyAPI(BaseModel):
     name: str
     industry: str
     starting_date: str
+    metadata: str
+    statement: bool
+    broke_promise: bool
 
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
